ec3b1cd10f8ef9e8d0705477e88e3bac9de11a67
Jacob Menashi <jbmenashi@gmail.com>
2024-06-07 20:30:16 -0500

    super basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,33 @@
+import { BiHome } from "react-icons/bi"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  const { user, userName } = useSelector((state) => state.user)
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <Link to="/" className="ml-6 link link-hover">
+        <BiHome />
+      </Link>
+      <Link to="/howitworks" className="ml-10 link link-hover">
+        How It Works
+      </Link>
+      {user ? (
+        <div>
+          <p>Hello {userName}</p>
+          <button className="btn btn-accent">Logout</button>
+        </div>
+      ) : (
+        <>
+          <Link to="/register" className="ml-10 link link-hover">
+            Register
+          </Link>
+          <Link to="/login" className="ml-10 link link-hover">
+            Login
+          </Link>
+        </>
+      )}
+    </div>
+  )
 }
 export default Navbar
--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   user: false,
+  userName: "",
 }
 
 const userSlice = createSlice({
