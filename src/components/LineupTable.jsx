7a07f5535041d7de904176a233299355a02aa2d7
Jacob Menashi <jbmenashi@gmail.com>
2024-07-22 21:58:30 -0400

    added rest of columns for lineup table

--- src/components/LineupTable.jsx
+++ src/components/LineupTable.jsx
@@ -68,8 +68,8 @@ const LineupTable = ({ selections, statistics }) => {
       <table className="table w-full border-collapse border border-primary">
         <thead>
           <tr>
-            <th className="border border-primary px-1"></th>
-            <th className="border border-primary px-20 py-2">Player</th>
+            <th className="border border-primary px-2">Position</th>
+            <th className="border border-primary px-28 py-2">Player</th>
             <th className="border border-primary px-4 py-2">Team</th>
             <th className="border border-primary px-4 py-2">Score</th>
             {statColumns.map((col) => {
@@ -86,20 +86,67 @@ const LineupTable = ({ selections, statistics }) => {
             if ("player_id" in sel) {
               return (
                 <tr key={sel.index}>
-                  <td className="border border-primary px-4 py-2 font-extrabold">{sel.position}</td>
-                  <td className="border border-primary px-4 py-2 font-extrabold">
+                  <td className="border border-primary px-4 py-2 font-extrabold text-base">{sel.position}</td>
+                  <td className="border border-primary px-4 py-2 font-extrabold text-lg">
                     {sel.first_name + " " + sel.last_name}
+                  </td>
+                  <td className="border border-primary px-4 py-2 font-extrabold">{sel.team_abbreviation}</td>
+                  <td className="border border-primary px-4 py-2 font-extrabold">
+                    {sel.total_points ? sel.total_points : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.pass_yds : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.pass_tds : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">{sel.fantasy_stats ? sel.fantasy_stats.ints : 0}</td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.rush_yds : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.receptions : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.rec_yds : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">{sel.fantasy_stats ? sel.fantasy_stats.tds : 0}</td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.fumbles : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.two_pt_conv : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_sacks : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_fum_rec : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_int : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_blk_kicks : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_safeties : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_tds_scored : 0}
+                  </td>
+                  <td className="border border-primary px-4 py-2">
+                    {sel.fantasy_stats ? sel.fantasy_stats.def_pts_allowed : 0}
                   </td>
                 </tr>
               )
             }
             return (
               <tr key={sel.index}>
-                <td className="border border-primary px-4 py-2 font-extrabold">{sel.position}</td>
+                <td className="border border-primary px-4 py-2 font-extrabold text-base">{sel.position}</td>
                 <td className="border border-primary px-4 py-2">
                   <Link
                     to={`/leagues/${leagueId}/teams/${teamId}/playersearch?position=${sel.position}`}
-                    className="btn btn-ghost"
                     onClick={() => handleStartSelection(lineup, sel.index, sel.position)}
                   >
                     <span className="text-lg text-primary underline">Select Player</span>
