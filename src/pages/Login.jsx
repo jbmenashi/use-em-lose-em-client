5e6c353e57e7e9ec0cd0e0a767e18b651d24e6e7
Jacob Menashi <jbmenashi@gmail.com>
2024-08-05 22:38:34 -0400

    removing env attempt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,13 +11,13 @@ export const action =
     const formData = await request.formData()
     const data = Object.fromEntries(formData)
     try {
-      const res = await axios.post(`${import.meta.env.BACKEND_URL}/auth/login`, data, {
+      const res = await axios.post(`https://use-em-lose-em-server-bd575796a9b2.herokuapp.com/auth/login`, data, {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
         },
       })
       if (res.status === 200) {
-        const res2 = await axios.get(`${import.meta.env.BACKEND_URL}/users/me`, {
+        const res2 = await axios.get(`https://use-em-lose-em-server-bd575796a9b2.herokuapp.com/users/me`, {
           headers: {
             Authorization: `Bearer ${res.data.access_token}`,
           },
