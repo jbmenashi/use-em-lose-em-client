d897e60b535a774dfc020f29a864b96e80774e52
Jacob Menashi <jbmenashi@gmail.com>
2024-10-07 21:17:46 -0500

    some styling on matchup list

--- src/components/ScheduleMatchup.jsx
+++ src/components/ScheduleMatchup.jsx
@@ -24,6 +24,71 @@ const ScheduleMatchup = (matchup) => {
     dispatch(falseIsUserMatchup())
   }
 
+  let nameStyling = ""
+  let scoreStyling = ""
+  let nameStylingTwo = ""
+  let scoreStylingTwo = ""
+
+  // if the matchup is over
+  if (finished) {
+    // if it's the user's team
+    if (team_1_id === teamId) {
+      // if the team won
+      if (team_1_id === winner) {
+        nameStyling = "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4 bg-blue-200"
+        scoreStyling = "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
+        nameStylingTwo = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+        scoreStylingTwo = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
+      }
+      // if the team didn't win
+      else {
+        nameStyling = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 bg-blue-200"
+        scoreStyling = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
+        nameStylingTwo = "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4 "
+        scoreStylingTwo = "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4 "
+      }
+    }
+    // if it's not the user's team
+    else {
+      // if the team won
+      if (team_1_id === winner) {
+        nameStyling = "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4"
+        scoreStyling = "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4"
+        nameStylingTwo = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+        scoreStylingTwo = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
+      }
+      // if the team didn't win
+      else {
+        nameStyling = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+        scoreStyling = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
+        nameStylingTwo = "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4"
+        scoreStylingTwo = "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4"
+      }
+    }
+  }
+  // if it's not over
+  else {
+    // if it's the user's team
+    if (team_1_id === teamId) {
+      nameStyling = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 bg-blue-200"
+      scoreStyling = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
+      nameStylingTwo = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+      scoreStylingTwo = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 "
+    } else if (team_2_id === teamId) {
+      nameStyling = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 "
+      scoreStyling = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 "
+      nameStylingTwo = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 bg-blue-200"
+      scoreStylingTwo = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
+    }
+    // if it's not the user's team
+    else {
+      nameStyling = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+      scoreStyling = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
+      nameStylingTwo = "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
+      scoreStylingTwo = "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
+    }
+  }
+
   return (
     <Link
       className="card bg-base-100 shadow-xl my-3 w-full sm:w-5/6"
@@ -34,53 +99,13 @@ const ScheduleMatchup = (matchup) => {
         <div className="flex items-center justify-center">
           <div className="grid grid-rows-2 grid-cols-5 w-full max-w-full sm:max-w-screen-md h-auto sm:h-32">
             {/* Team 1 Name */}
-            <div
-              className={
-                team_1_id === teamId
-                  ? finished && team_1_id === winner
-                    ? "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4 bg-blue-200"
-                    : "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 bg-blue-200"
-                  : "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
-              }
-            >
-              {team_1_name}
-            </div>
+            <div className={nameStyling}>{team_1_name}</div>
             {/* Team 1 Score */}
-            <div
-              className={
-                team_1_id === teamId
-                  ? finished && team_1_id === winner
-                    ? "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
-                    : "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
-                  : "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
-              }
-            >
-              {team_1_score.toFixed(2)}
-            </div>
+            <div className={scoreStyling}>{team_1_score.toFixed(2)}</div>
             {/* Team 2 Name */}
-            <div
-              className={
-                team_2_id === teamId
-                  ? finished && team_2_id === winner
-                    ? "col-span-4 text-lg sm:text-2xl font-bold p-2 sm:p-4 bg-blue-200"
-                    : "col-span-4 text-lg sm:text-2xl p-2 sm:p-4 bg-blue-200"
-                  : "col-span-4 text-lg sm:text-2xl p-2 sm:p-4"
-              }
-            >
-              {team_2_name}
-            </div>
+            <div className={nameStylingTwo}>{team_2_name}</div>
             {/* Team 2 Score */}
-            <div
-              className={
-                team_2_id === teamId
-                  ? finished && team_2_id === winner
-                    ? "col-span-1 text-lg sm:text-2xl font-bold px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
-                    : "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4 bg-blue-200"
-                  : "col-span-1 text-lg sm:text-2xl px-3 sm:px-5 py-2 sm:py-4"
-              }
-            >
-              {team_2_score.toFixed(2)}
-            </div>
+            <div className={scoreStylingTwo}>{team_2_score.toFixed(2)}</div>
           </div>
         </div>
       </div>
