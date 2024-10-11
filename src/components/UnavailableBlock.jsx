8c2d075a82664b399925d81c6eff38257ce4587c
Jacob Menashi <jbmenashi@gmail.com>
2024-10-11 16:05:25 -0500

    unavailable block now has more info

--- src/components/UnavailableBlock.jsx
+++ src/components/UnavailableBlock.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router-dom"
 
 const UnavailableBlock = () => {
   const { contestant } = useLoaderData()
+  console.log(contestant)
   let players = ""
   for (let player of contestant.unavailable_players) {
     players = players + player.player_name + ", "
@@ -12,13 +13,28 @@ const UnavailableBlock = () => {
     teams = teams + team.team_abbreviation + ", "
   }
 
+  // Convert object to an array of key-value pairs
+  let sortedTeamCountArray = Object.entries(contestant.team_count).sort((a, b) => b[1] - a[1])
+
+  // Convert sorted array back into an object
+  let sortedTeamCount = Object.fromEntries(sortedTeamCountArray)
+
+  let teamCount = ""
+  for (let [key, value] of Object.entries(sortedTeamCount)) {
+    teamCount = teamCount + `${key}: ${value}, `
+  }
+
+  console.log(teamCount)
   return (
-    <div className="max-h-[5rem] overflow-y-auto">
+    <div className="max-h-[5rem] sm:max-h-[7rem] overflow-y-auto">
       <p className="line-clamp-none">
-        <span className="font-bold">Unavailable Players:</span> {players}
+        <span className="font-bold">Current Team Count:</span> {teamCount}
       </p>
       <p className="line-clamp-none">
         <span className="font-bold">Unavailable Teams:</span> {teams}
+      </p>
+      <p className="line-clamp-none">
+        <span className="font-bold">Unavailable Players:</span> {players}
       </p>
     </div>
   )
